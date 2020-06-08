124c896b2402a0f0543788ffd0081fe28e156332
Hagen E. Fritz <hagenfritz@gmail.com>
2020-06-08 10:45:33 -0500

    function to combine studies

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,22 +1,50 @@
 # -*- coding: utf-8 -*-
-import click
+import pandas as pd
+import numpy as np
+import os
 import logging
 from pathlib import Path
 
+def combine_across_studies(dir_string='fitbit',file_string='dailySteps_merged'):
+    '''
+    Imports fitbit or beiwe mood data from ut1000 and ut2000 studies and combines into one dataframe
+    '''
+    df = pd.DataFrame()
+    for i in range(2):
+        temp = pd.read_csv(f'../../data/raw/ut{i+1}000/{dir_string}/{file_string}.csv')
+        temp['study'] = f'ut{i+1}000'
+        
+        crossover = pd.read_csv(f'../../data/raw/ut{i+1}000/admin/id_crossover.csv')
+        if 'Id' in temp.columns:
+            temp = pd.merge(left=temp,right=crossover,left_on='Id',right_on='record',how='left')
+        elif 'pid' in temp.columns:
+            temp = pd.merge(left=temp,right=crossover,left_on='pid',right_on='beiwe',how='left')
+        else:
+            return False
+        
+        df = pd.concat([df,temp])
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
+    df.to_csv(f'../../data/processed/ut3000_{dir_string}_{file_string}.csv')
+        
+    return True
+
+def main():
+    '''
+    Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
-    """
+    '''
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+    modality='fitbit'
+    var_='dailySteps_merged'
+    if combine_across_studies(modality, var_):
+        logger.info(f'Data for {modality} {var_} processed')
+    else:
+        logger.error(f'Data for {modality} {var_} NOT processed')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.basicConfig(filename='dataset.log', filemode='w', level=logging.DEBUG, format=log_fmt)
 
     main()
