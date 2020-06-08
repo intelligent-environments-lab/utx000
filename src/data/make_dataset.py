095a6e777db528c67502e120af4b44bac756b358
Hagen E. Fritz <hagenfritz@gmail.com>
2020-06-08 11:34:28 -0500

    included processing for sleep data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,24 +7,37 @@ from pathlib import Path
 
 def combine_across_studies(dir_string='fitbit',file_string='dailySteps_merged'):
     '''
-    Imports fitbit or beiwe mood data from ut1000 and ut2000 studies and combines into one dataframe
+    Imports fitbit or beiwe mood data from ut1000 and ut2000, combines them into
+    one dataframe, then adds/adjusts columns before writing data to a csv in the
+    processed data directory.
     '''
     df = pd.DataFrame()
     for i in range(2):
+    	# import the file and attach a study tag
         temp = pd.read_csv(f'../../data/raw/ut{i+1}000/{dir_string}/{file_string}.csv')
         temp['study'] = f'ut{i+1}000'
         
+        # import the id crossover file and attach so we have record, beiwe, and beacon id
         crossover = pd.read_csv(f'../../data/raw/ut{i+1}000/admin/id_crossover.csv')
-        if 'Id' in temp.columns:
+        if 'Id' in temp.columns: # fitbit
             temp = pd.merge(left=temp,right=crossover,left_on='Id',right_on='record',how='left')
-        elif 'pid' in temp.columns:
+        elif 'pid' in temp.columns: # beiwe
             temp = pd.merge(left=temp,right=crossover,left_on='pid',right_on='beiwe',how='left')
-        else:
+        else: # neither
             return False
         
         df = pd.concat([df,temp])
 
-    df.to_csv(f'../../data/processed/ut3000_{dir_string}_{file_string}.csv')
+    # further processessing based on dir and file strings
+    if dir_string == 'fitbit' and file_string == 'sleepStagesDay_merged':
+    	# removing nights that have no measured sleep
+    	df = df[df['TotalMinutesLight'] > 0]
+    	# adding extra sleep metric columns
+    	df['SleepEfficiency'] = df['TotalMinutesAsleep'] / df['TotalTimeInBed']
+    	df['TotalMinutesNREM'] = df['TotalMinutesLight'] + df['TotalMinutesDeep'] 
+    	df['REM2NREM'] = df['TotalMinutesREM'] / df['TotalMinutesNREM']
+
+    df.to_csv(f'../../data/processed/ut3000_{dir_string}_{file_string}.csv',index=False)
         
     return True
 
@@ -36,7 +49,7 @@ def main():
     logger = logging.getLogger(__name__)
 
     modality='fitbit'
-    var_='dailySteps_merged'
+    var_='sleepStagesDay_merged'
     if combine_across_studies(modality, var_):
         logger.info(f'Data for {modality} {var_} processed')
     else:
