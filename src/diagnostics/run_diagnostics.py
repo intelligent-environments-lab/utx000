a8506541abad5b3453c28e774a9cfcb93a67507c
Hagen E. Fritz <hagenfritz@gmail.com>
2020-12-27 09:32:54 -0600

    rough draft

--- src/diagnostics/run_diagnostics.py
+++ src/diagnostics/run_diagnostics.py
@@ -7,123 +7,114 @@ import numpy as np
 from datetime import datetime, timedelta
 
 class Diagnostics():
-	def __init__(self, single=False, save_dir="../../data/raw/bpeace2/beacon/"):
-		if single:
-			self.beacon = input("Enter Beacon Number (0,50): ") # setting to user input
-			try:
-				if int(self.beacon) < 10:
-					self.beacon = '0' + str(self.beacon)
-			except ValueError:
-				print("You must enter a valid number")
-				self.terminate()
-		else:
-			self.beacon = "All" # setting to dummy
 
-		self.single = single
-		self.save_dir = save_dir
+    def __init__(self, single=False, save_dir="../../data/raw/bpeace2/beacon/"):
+        """
+        Checks if we want a single beacon, otherwise runs for all. User can
+        download, remove, or run diagnostics.
+        """
 
-		op1 = input(f"Run Diagnostics on Beacon {self.beacon} (y/n)? ")
-		if op1.lower() in ["y","yes"]:
-			print(f"WARNING: Running diagnostics will remove all data from the selected Beacons and save the data to this directory:\n\t{self.save_dir}")
-			proceed = input("Do you still wish to proceed (y/n)? ")
-			if proceed.lower() in ["y","yes"]:
-				self.run()
-			else:
-				self.terminated()
-		else:
-			op2 = input(f"Download data from Beacon {self.beacon} only (y/n)? ")
-			if op2.lower() in ["y","yes"]:
-				self.downloadData(save_dir=self.save_dir)
-			else:
-				op3 = input(f"Download data from Beacon {self.beacon} and DELETE after receiving (y/n)? ")
-				if op3.lower() in ["y","yes"]:
-					print(f"WARNING: Running this option will remove all data from the selected Beacons and save the data to the following directory:\n\n\t{self.save_dir}")
-					proceed = input("\nDo you still wish to proceed (y/n)? ")
-					if proceed.lower() in ["y","yes"]:
-						self.downloadData(remove=True,save_dir=self.save_dir)
-					else:
-						self.terminated()
-				else:
-					self.terminated()
+        # Getting Beacon Numbers
+        # ----------------------
+        if single:
+            self.beacon = input("Enter Beacon Number (0,50): ") # setting to user input
+            try:
+                if int(self.beacon) < 10:
+                    self.beacon = [self.beacon] # single beacon number - 0 will be added in run()
+            except ValueError:
+                print("You must enter a valid number")
+                self.terminate()
+        else:
+            self.beacon = range(0,51) # setting to list of all beacons
 
+        self.save_dir = save_dir # directory to save downloaded data to
 
-	def downloadData(self, remove=False, save_dir="../../data/raw/bpeace2/beacon/"):
-		"""
-		Downloads data from Beacons
+        # Commands
+        # ------------
+        # Run diagnostics
+        op1 = input(f"Run Diagnostics on Beacon {self.beacon} (y/n)? ")
+        if op1.lower() in ["y","yes"]:
+            print(f"WARNING: Running diagnostics will remove all data from the selected Beacons and save the data to this directory:\n\t{self.save_dir}")
+            proceed = input("Do you still wish to proceed (y/n)? ")
+            if proceed.lower() in ["y","yes"]:
+                fxns = [self.getUpdates,self.downloadData,self.removeData,self.checkSensors]
+                self.run(fxns)
+            else:
+                self.terminated()
+        else:
+            # Download data
+            op2 = input(f"Download data from Beacon {self.beacon} only (y/n)? ")
+            if op2.lower() in ["y","yes"]:
+                fxns = [self.downloadData]
+                self.run(fxns)
+            else:
+                # Download AND remove data
+                op3 = input(f"Download data from Beacon {self.beacon} and DELETE after receiving (y/n)? ")
+                if op3.lower() in ["y","yes"]:
+                    print(f"WARNING: Running this option will remove all data from the selected Beacons and save the data to the following directory:\n\n\t{self.save_dir}")
+                    proceed = input("\nDo you still wish to proceed (y/n)? ")
+                    if proceed.lower() in ["y","yes"]:
+                        fxns = [self.downloadData, self.removeData]
+                        self.run(fxns)
+                    else:
+                        self.terminated()
+                else:
+                    self.terminated()
 
-		Inputs:
-		- remove: boolean specifying whether or not to delete data afterwards
-		- save_dir: string specifying where to save the data
+    def getUpdates(self, beacon_no):
+        """
+        Pulls in updates from git
+        """
+        print("\n\tUpdating from Git:")
+        os.system(f'ssh pi@iaq{beacon_no} -o ConnectTimeout=1 "sudo apt-get install pigpio python-pigpio python3-pigpio & cd bevo_iaq & git reset --hard & git pull & python3 fixnumber.py"')
 
-		Returns void
-		"""
-		os.system("clear")
-		print("Downloading Data...")
-		if self.single:
-			os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{self.beacon}:/home/pi/DATA/adafruit/ {save_dir}B{self.beacon}/')
-			os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{self.beacon}:/home/pi/DATA/sensirion/ {save_dir}B{self.beacon}/')
-		else:
-			for i in range(0,51):
-				if i < 10:
-					i = '0' + str(i)
+    def downloadData(self, beacon_no, save_dir="../../data/raw/bpeace2/beacon/"):
+        """
+        Downloads data from specified beacon
+        """
+        print("\n\tDownloading Data:")
+        os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{beacon_no}:/home/pi/DATA/adafruit/ {save_dir}B{beacon_no}/')
+        os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{beacon_no}:/home/pi/DATA/sensirion/ {save_dir}B{beacon_no}/')
 
-				print(f"Beacon {i}:")
-				os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{i}:/home/pi/DATA/adafruit/ {save_dir}B{i}/')
-				os.system(f'scp -r -o ConnectTimeout=1 pi@iaq{i}:/home/pi/DATA/sensirion/ {save_dir}B{i}/')
+    def removeData(self, beacon_no):
+        """
+        Removes data from specified beacon
+        """
+        print("\n\tDeleting Data:")
+        #os.system('tput setaf 1; echo REMOVING LOG2/LOG3 DATA; tput sgr 0')
+        os.system(f'ssh pi@iaq{beacon_no} -o ConnectTimeout=1 "cd DATA/adafruit/ & sudo rm *.csv & cd ../sensirion/ & sudo rm *.csv"')
 
-		if remove:
-			self.removeData()
+    def checkSensors(self, beacon_no):
+        """
+        Checks the operation of the sensors
+        """
+        print("\n\tChecking Sensor Connection:")
+        os.system(f'ssh pi@iaq{beacon_no} -o ConnectTimeout=1 "python3 bevo_iaq/Setup/Code/addresses.py"')
 
-	def removeData(self):
-		"""
-		Removes data from Beacons
+    def run(self,fxns):
+        """
+        Runs the functions in fxns
+        """
+        for i in self.beacon:
+            if i < 10:
+                i = '0' + str(i)
+            print(f"Beacon {i}:")
+            for f in fxns:
+                f(i)
 
-		Returns void
-		"""
-		os.system("clear")
-		print("Removing Data...")
-		if self.single:
-			os.system('tput setaf 1; echo REMOVING LOG2 DATA; tput sgr 0')
-			os.system(f'ssh pi@iaq{self.beacon} -o ConnectTimeout=10 "cd DATA/adafruit/ && sudo rm *.csv"')
-			os.system('tput setaf 1; echo REMOVING LOG3 DATA; tput sgr 0')
-			os.system(f'ssh pi@iaq{self.beacon} -o ConnectTimeout=10 "cd DATA/sensirion/ && sudo rm *.csv"')
-		else:
-			for i in range(0,51): # looping through all the beacons
-				if i < 10:
-					i = '0' + str(i)
-
-				print(f"Beacon {i}:")
-				print('\tDeleting log2 data')
-				os.system(f'ssh pi@iaq{i} -o ConnectTimeout=10 "cd DATA/adafruit/ && sudo rm *.csv"')
-				print('\tDeleting log3 data')
-				os.system(f'ssh pi@iaq{i} -o ConnectTimeout=10 "cd DATA/sensirion/ && sudo rm *.csv"')
-
-	def checkSensors(self):
-		"""
-		Checks the operation of the sensors
-		"""
-		os.system(f'ssh pi@iaq{self.beacon} -o ConnectTimeout=10 "python3 bevo_iaq/Setup/Code/addresses.py"')
-
-	def run(self):
-		"""
-		Runs diagnostics
-		"""
-		self.checkSensors()
-
-	def terminated(self):
-		print("User has terminated program.")
-		os.system("clear")
-		return False
+    def terminated(self):
+        print("User has terminated program.")
+        return False
 
 def main():
-	logger = logging.getLogger(__name__)
-	os.system("clear")
+    logger = logging.getLogger(__name__)
+    os.system("clear")
 
-	single = input("Individual Download (y/n)? ")
-	if single.lower() in ["y","yes"]:
-		d = Diagnostics(True)
-	else:
-		d = Diagnostics(False)
+    single = input("Individual Download (y/n)? ")
+    if single.lower() in ["y","yes"]:
+        d = Diagnostics(True)
+    else:
+        d = Diagnostics(False)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
