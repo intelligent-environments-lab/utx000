421a513d6a75a20cf3ac534cbed5dd56a0b4f4df
Hagen E. Fritz <hagenfritz@gmail.com>
2021-02-26 08:05:51 -0600

    new visuals

--- src/processing/process.py
+++ src/processing/process.py
@@ -71,3 +71,74 @@ class preprocess():
             for column in df.columns:
                 df[column] = stats.yeojohnson(df[column])[0]
         return df
+
+    def plot_missing_data(self,df):
+        """
+        Plots the number of missing values
+
+        Returns void
+        """
+        missing_vals = df.isnull().sum()
+        vals = missing_vals[missing_vals > 0].sort_values()
+        # setting up bar chart
+        locs = np.arange(len(vals))
+        ticks = list(vals.index)
+        formatted_ticks = []
+        for tick in ticks:
+            formatted_ticks.append(tick.replace("_", " ").title())
+        my_cmap = plt.get_cmap("Blues")
+        rescale = lambda y: y / np.max(y)
+
+        _, ax = plt.subplots(figsize=(8,5))
+        rects = ax.barh(locs, vals, color=my_cmap(rescale(vals)), edgecolor="black")
+        # formatting y-axis
+        plt.yticks(locs, formatted_ticks)
+        # formatting remainder
+        n = len(df)
+        ax.set_title(f"Missing Values - Total Values: {n}")
+
+        for spine_loc in ["top","right"]:
+            ax.spines[spine_loc].set_visible(False)
+            
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                width = rect.get_width()
+                ax.annotate('{}'.format(width),
+                            xy=(width, rect.get_y()),
+                            xytext=(3, 0),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='left', va='bottom')
+                
+        autolabel(rects)
+
+        plt.show()
+        plt.close()
+
+    def plot_correlation_matrix(self,df,annotate=True):
+        """
+        Plots correlation matrix between all variables in the df
+        
+        Inputs:
+        - df: dataframe with columns named for the varaiables
+        
+        """
+        corr = df.corr()
+        corr = round(corr,2)
+        #mask = np.triu(np.ones_like(corr, dtype=bool))
+        _, ax = plt.subplots(figsize=(9, 7))
+        sns.heatmap(corr,
+                        vmin=-1, vmax=1, center=0, 
+                        cmap=sns.diverging_palette(20, 220, n=200),cbar_kws={'ticks':[-1,-0.5,0,0.5,1]},
+                        square=True,linewidths=0.5,linecolor="black",annot=annotate,ax=ax)
+
+        yticklabels = ax.get_yticklabels()
+        yticklabels[0] = ' '
+        ax.set_yticklabels(yticklabels,rotation=0,ha='right')
+
+        xticklabels = ax.get_xticklabels()
+        xticklabels[-1] = ' '
+        ax.set_xticklabels(xticklabels,rotation=-45,ha='left')
+            
+        plt.show()
+        plt.close()
